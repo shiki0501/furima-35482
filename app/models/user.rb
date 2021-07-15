5997c2afa8ebc78ac207283a2fa471552aadb0fb
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-07-15 21:11:28 +0900

    ユーザー管理機能（バリテーション　々の追加）

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\Z/
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-  VALID_KANJI_REGEX = /\A([ぁ-んァ-ヶ一ー-龥]+)\Z/
+  VALID_KANJI_REGEX = /\A([ぁ-んァ-ヶ一ー-龥々]+)\Z/
   validates :first_name_kanji, format: { with: VALID_KANJI_REGEX }
   validates :last_name_kanji, format: { with: VALID_KANJI_REGEX }
 
