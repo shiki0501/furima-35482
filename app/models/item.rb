361c20d2f0ee9165a5011979fa965bbc49be3937
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-07-18 15:31:11 +0900

    Active Storangeの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
 end
