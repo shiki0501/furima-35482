e913bd7e5e8ee338d2a2f75fd7eaf8c85d2207d9
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-07-18 18:36:14 +0900

    販売手数料や販売利益の計算

---
+++ app/javascript/price.js
@@ -0,0 +1,14 @@
+function post (){
+  const price = document.getElementById("item-price")
+  addEventListener("input", () => {
+      const InputPrice = price.value
+      const PriceTax = document.getElementById("add-tax-price")
+      const Tax = 0.1
+      PriceTax.innerHTML = Math.floor(price.value*Tax)
+      const PriceProfit = document.getElementById("profit")
+      PriceProfit.innerHTML = price.value-PriceTax.innerHTML
+    console.log(PriceTax)
+  });
+};
+
+window.addEventListener("load", post);
