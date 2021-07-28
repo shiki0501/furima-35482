974018c05e4ef67d6f8494914761294a8cafc8fc
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-07-28 18:32:00 +0900

    商品一覧　修正
    出品順に並べる
    不要なコメントアウト削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = Item.all
-    # binding.pry
-    # @item = Item.order('created_at DESC')
+    @items = Item.order('created_at DESC')
   end
 
   def new
