218ef7ffd088438b1fd3125f2e0c38ffa8125c81
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-08-01 15:31:07 +0900

    編集機能実装
    editアクションの@item定義
    updateアクションの記述追加
    edit.erb.htmlの各データ読み出し記述修正
    routes updateアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,19 +9,29 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
-
-  def edit
-  end
-
   def create
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
     end
   end
 
