3f954b6b03a14157e12c1beb020d6b2c0e27fe4d
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-08-15 18:25:56 +0900

    API導入２
    ・10〜15
    フォーム画面から入力させた情報をcardへ入れる。
    ・18〜23
    トークン情報をフォームに追加
    ・26〜２９
    クレジット情報の削除
    入力された各情報を削除（セキュリティ性アップ）

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,33 @@ const pay = () => {
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
   });
 };
 
