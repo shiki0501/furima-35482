73abc99ca3424372b4333cb46ef4db9a1cf8f795
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-08-15 17:02:23 +0900

    API導入
    javascriptの記述修正
    ・手作業でJavaScriptを記述してフォーム送信処理などを実装する場合は、このturbolinksがその処理を無効にしてしまうことがあるため、turbolinksの記述を削除
    card.jsファイルの追加

---
+++ app/javascript/card.js
@@ -0,0 +1,10 @@
+const pay = () => {
+  Payjp.setPublicKey("");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("フォーム送信時にイベント発火")
+  });
+};
+
+window.addEventListener("load", pay);
