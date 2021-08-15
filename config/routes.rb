16f880f8c167032b057fd0431a86bf37ef35c114
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-08-15 12:17:21 +0900

    コードレビュー後の修正
    routes
    ・resourcesにて７つのアクションをまとめて出す。(７つ全部使うためonlyで制限する必要がない）

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   
-  resources :items, only: [:index, :create, :new, :show,:edit,:update,:destroy]
-
+  resources :items
 end
