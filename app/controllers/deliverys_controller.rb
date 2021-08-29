421d6ca5b44eebc93bbf996dea0e71b97a2b76df
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-08-29 11:04:32 +0900

    クレジットカード購入機能実装
    ・payjp導入
    ・card.jp記述変更
    　name属性変更（deleivery_buy)
    ・controller記述追加
    　Payjpへ売り上げが反映される。
    
    ※現状はエラー発生し、データベースへは保存されない。

--- app/controllers/deliverys_controller.rb
+++ app/controllers/deliverys_controller.rb
@@ -6,8 +6,15 @@ class DeliverysController < ApplicationController
   end
   
   def create
-    @delivery_buy = DeliveryBuy.new(deliverybuy_params)
+    @item = Item.find(params[:item_id])
+    @delivery_buy = DeliveryBuy.new(delivery_params)
     if @delivery_buy.valid?
+      Payjp.api_key = ""
+      Payjp::Charge.create(
+        amount: @item[:price],
+        card: @delivery_buy.token,
+        currency: 'jpy'
+      )
       @delivery_buy.save
       return redirect_to root_path
     else
@@ -18,8 +25,13 @@ class DeliverysController < ApplicationController
 
   private
 
-  def deliverybuy_params
-    params.require(:delivery_buy).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :tell) 
+  def delivery_params
+    params.require(:delivery_buy).permit(:postal_code, :shipping_area_id, :municipality, :address, :building_name, :tell,)
+    .merge(token: params[:token])
+  end
+
+  def buy_params
+    params.require(:delivery_buy).permit(:price)
   end
 
 end
--- app/models/delivery_buy.rb
+++ app/models/delivery_buy.rb
@@ -3,17 +3,17 @@ class DeliveryBuy
   attr_accessor :postal_code,:shipping_area_id,:municipality,:address,:building_name,:tell,:buy_id,:user_id,:item_id,:token
 
   def save
-    buy = Buy.create(user_id: user_id, item_id: item_id)
-    Delivery.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, tell: tell, buy_id: buy_id)
+    buy = Buy.create(user_id: current_user.id, item_id: @item.id)
+    Delivery.create(postal_code: postal_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, building_name: building_name, tell: tell, token: token, buy_id: buy_id)
   end
 
   with_options presence: true do
     validates :token
-    validates :postal_code, format: {with: /\A\d{3}[-]\d{2}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ , message: "is invalid. Include hyphen(-)"}
     validates :shipping_area_id
     validates :municipality
     validates :address
-    validates :tell, format: {with: /\A\d{11}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :tell, format: {with: /\A\d{11}\z/ , message: "is invalid. Include hyphen(-)"}
   end
 
 end
